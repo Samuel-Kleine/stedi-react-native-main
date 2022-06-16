a39c699aab31af8cb70b9db83626f1c5d39d7d83
Samuel-Kleine <97302079+Samuel-Kleine@users.noreply.github.com>
2022-06-16 13:51:46 -0600

    Added the function for the button to send a text

--- Login.js
+++ Login.js
@@ -1,6 +1,17 @@
 import {useState} from "react";
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, TextInput } from "react-native";
 
+const sendText = async (phoneNumber) => {
+  //Using fetch do a POST to https://dev.stedi.me/twofactorlogin/+phoneNumber
+  await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/text'
+    }
+  });
+  console.log("PhoneNumber is:",phoneNumber);
+};
+
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState(" ");
   const [oneTimePassword, setOneTimePassword] = useState(null);
@@ -12,17 +23,26 @@ const Login = () => {
         onChangeText={setPhoneNumber}
         value={phoneNumber}
         placeholder="801-867-5309"
+        placeholderTextColor='#808080'
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Send Text</Text>
+      </TouchableOpacity>
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1111"
+        placeholderTextColor='#808080'
         keyboardType="numeric"
         secureTextEntry={true}
       />  
       <TouchableOpacity
         style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
       >
         <Text>Login</Text>
       </TouchableOpacity>
@@ -36,7 +56,7 @@ const styles = StyleSheet.create({
       margin: 12,
       borderWidth: 1,
       color: '#000000',
-      backgroundColor: '#fd5e53',
+      backgroundColor: '#87ceeb',
       padding: 10,
     },
     mainView: {
